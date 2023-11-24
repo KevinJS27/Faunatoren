9b16579d4d6947f3bfdbf8455b05736a5e4994d6
KevinJS27 <kj.schollen@student.avans.nl>
2023-11-25 00:34:12 +0100

    login & profile added

--- Faunatoren/src/main.jsx
+++ Faunatoren/src/main.jsx
@@ -13,7 +13,9 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Auth0Provider } from '@auth0/auth0-react';
 import App from './App';
-import LoginButton from './button';
+import LoginButton from './loginBtn';
+import LogoutButton from './logoutBtn';
+import Profile from './profile';
 
 const root = createRoot(document.getElementById('root'));
 
@@ -22,10 +24,13 @@ root.render(
     domain="kjschollen.eu.auth0.com"
     clientId="cernTRpK8SxCXwI69sA594JLHc6FlrU4"
     authorizationParams={{
-      redirect_uri: window.location.origin
+      redirect_uri: window.location.origin,
+      audience: "http://localhost:3000/",
+      scope: "read:current_user update:current_user_metadata"
     }}
   >
     <LoginButton />
-
+    <LogoutButton />
+    <Profile />
   </Auth0Provider>,
 );
