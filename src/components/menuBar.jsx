a55b52967ba2e73375a677ac433dd07ce19df947
Thijs Teerhuis <thijs@teerhuis.eu>
2023-12-15 13:22:29 +0100

    Removed unnecessary code

--- src/components/menuBar.jsx
+++ src/components/menuBar.jsx
@@ -7,30 +7,23 @@ import userIcon from './../assets/userIcon.svg'
 function MenuBar() {
 
     return (
-        <>
-            <div className="container-fluid menubar">
-                <div className="row">
-                    <div className="container">
-                        <div className="row">
-                            <menu className="col-sm menu">
-                                <img src={faunaTorenLogo} className="logo" alt="Faunatoren logo" />
-                                <li>Dashboard</li>
-                                <li>Torens</li>
-                                <li>Huisjes</li>
-                                <li>icon</li>
-                                <li><img src={userIcon} className="User" alt="Gebruiker informatie" /></li>
-                            </menu>
-                        </div>
+        <div className="container-fluid menubar">
+            <div className="row">
+                <div className="container">
+                    <div className="row">
+                        <menu className="col-sm menu">
+                            <img src={faunaTorenLogo} className="logo" alt="Faunatoren logo" />
+                            <li>Dashboard</li>
+                            <li>Torens</li>
+                            <li>Huisjes</li>
+                            <li>icon</li>
+                            <li><img src={userIcon} className="User" alt="Gebruiker informatie" /></li>
+                        </menu>
                     </div>
                 </div>
             </div>
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-6">Lorem ipsum dolor sit.</div>
-                    <div className="col-md-6">Lorem ipsum dolor sit amet.</div>
-                </div>
-            </div>
-        </>
+        </div>
+
     )
 }
 
