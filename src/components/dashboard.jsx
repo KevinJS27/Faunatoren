9cf455cd037792002caaa25490fcff5336b248c8
Thijs <thijs@teerhuis.eu>
2024-01-11 19:06:41 +0100

    WP dashboard

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -1,8 +1,7 @@
-import "./../style/components/dashboard.scss"
+import "./../style/components/dashboard.scss";
+import Chart from 'chart.js/auto'; // https://stackoverflow.com/questions/67727603/error-category-is-not-a-registered-scale
 import { Line } from "react-chartjs-2";
-import Chart from "chart.js/auto";
 import React, { useState, useEffect } from "react";
-
 
 let data = {
     airPressure: [],
@@ -10,54 +9,107 @@ let data = {
     weight: [],
     humidity: [],
     presences: [],
-    temperature: []
-}
+    temperature: [],
+};
 
 function Dashboard() {
-
     let [state, setState] = useState({
-        labels: ["Loading..."],
-        datasets: [
-            {
-                label: "Loading data",
-                backgroundColor: "rgb(255, 99, 132)",
-                borderColor: "rgb(255, 99, 132)",
-                data: [0, 10, 5, 2, 20, 30, 45],
-            },
-        ],
+        temperature: {
+            labels: ["Loading..."],
+            datasets: [
+                {
+                    label: "Loading data",
+                    borderColor: "black",
+                    data: [0, 10, 5, 2, 20, 30, 45],
+                },
+            ],
+        },
+        CO2: {
+            labels: ["Loading..."],
+            datasets: [
+                {
+                    label: "Loading data",
+                    borderColor: "black",
+                    data: [0, 10, 5, 2, 20, 30, 45],
+                },
+            ],
+        },
     });
 
-    function click() {
-        getData(setState);
+    useEffect(() => {
+        getData();
+    }, []); // Empty dependency array to run only on mount
 
-        // let labels = [];
-        // let values = [];
+    // Fetch data from the API
+    function getData() {
+        // Empty the arrays
+        data.airPressure = [];
+        data.CO2 = [];
+        data.weight = [];
+        data.humidity = [];
+        data.presences = [];
+        data.temperature = [];
 
-        // console.log(data);
-        // data.temperature.forEach(element => {
-        //     console.log(element);
+        // Get the data from the API
+        fetch("http://avans.duckdns.org:1880/sensor?limit=10")
+            .then((response) => response.json())
+            .then((result) => {
+                // Set the new data in the arrays
+                result.forEach((r) => {
+                    data.airPressure.push(r.decoded_payload.luminosity_3 / 100);
+                    data.CO2.push(r.decoded_payload.luminosity_5 * 10);
+                    data.weight.push(r.decoded_payload.luminosity_6 / 10);
+                    data.humidity.push(r.decoded_payload.relative_humidity_1);
+                    data.presences.push(r.decoded_payload.relative_humidity_4);
+                    data.temperature.push({
+                        key: r.time,
+                        value: r.decoded_payload.temperature_2 / 10,
+                    });
+                });
 
-        //     // Key (time)
-        //     let newDate = new Date(element.key);
-        //     console.log(newDate);
-        //     labels.push(newDate.getMonth() + " " + newDate.getDate());
+                // Update the temperature diagram
+                temperatureDiagramUpdate();
+            });
+    }
 
-        //     // Value
-        //     values.push(element.value);
+    // Update the temperature diagram
+    function temperatureDiagramUpdate() {
+        // Transform the data
+        let labels = [];
+        let values = [];
 
-        // });
+        data.temperature.forEach((element) => {
+            // Key (time)
+            let newDate = new Date(element.key);
+            labels.push(
+                newDate.getDate() +
+                "/" +
+                newDate.getMonth() +
+                1 +
+                " " +
+                newDate.getHours() +
+                ":" +
+                newDate.getMinutes()
+            );
 
-        // setState({
-        //     labels: labels,
-        //     datasets: [
-        //         {
-        //             label: "Temperatuur",
-        //             backgroundColor: "rgb(1, 1, 132)",
-        //             borderColor: "rgb(255, 99, 132)",
-        //             data: [100, 10, 5, 25, 23, 50, 55],
-        //         },
-        //     ],
-        // });
+            // Value
+            values.push(element.value);
+        });
+
+        // Update the state
+        setState((prevState) => ({
+            ...prevState,
+            temperature: {
+                ...prevState.temperature,
+                labels: labels,
+                datasets: [
+                    {
+                        label: "Temperatuur",
+                        data: values,
+                    },
+                ],
+            },
+        }));
     }
 
     return (
@@ -82,72 +134,47 @@ function Dashboard() {
                     </select>
                 </div>
             </div>
-            <Line className="temperature" data={state} />
-            <button onClick={click}>
-                Click me!
-            </button>
+
+            <div className="row">
+                {/* temp */}
+                <div className="col-md-4">
+                    <div className="wr-diagram">
+                        <h3>Temperatuur</h3>
+                        <div class="slidecontainer">
+                            <input type="range" min="1" max="100" value="80" class="slider" id="myRange"/>
+                        </div>
+                    </div>
+                </div>
+
+                {/* Pressure */}
+                <div className="col-md-4">
+                    <div className="wr-diagram">
+                        <h3>Druk</h3>
+                    </div>
+                </div>
+
+                {/* CO2 */}
+                <div className="col-md-4">
+                    <div className="wr-diagram">
+                        <h3>Co2</h3>
+                    </div>
+                </div>
+
+            </div>
+
+            <div className="row">
+                {/* Temp line chart */}
+                <div className="col-xl-6 col-sm-12">
+                    <div className="wr-diagram">
+                        <h3>Temperatuur</h3>
+                        <Line className="temperature" data={state.temperature} />
+                    </div>
+                </div>
+            </div>
+
+
         </div>
-    )
-};
-
-function getData(setState) {
-    // Empty the arrays
-    data.airPressure = [];
-    data.CO2 = [];
-    data.weight = [];
-    data.humidity = [];
-    data.presences = [];
-    data.temperature = [];
-
-
-    // Get the data from the API
-    fetch('http://avans.duckdns.org:1880/sensor?limit=10')
-        .then(
-            response => response.json()
-        )
-        .then(result => {
-
-            // Set the new data in the arrays
-            result.forEach(r => {
-                data.airPressure.push(r.decoded_payload.luminosity_3 / 100);
-                data.CO2.push(r.decoded_payload.luminosity_5 * 10);
-                data.weight.push(r.decoded_payload.luminosity_6 / 10);
-                data.humidity.push(r.decoded_payload.relative_humidity_1)
-                data.presences.push(r.decoded_payload.relative_humidity_4);
-                data.temperature.push({ key: r.time, value: r.decoded_payload.temperature_2 / 10 });
-            });
-
-            // Set in temperature diagram
-            // Transform the data
-            let labels = [];
-            let values = [];
-
-            data.temperature.forEach(element => {
-                console.log(element);
-
-                // Key (time)
-                let newDate = new Date(element.key);
-                console.log(newDate);
-                labels.push(newDate.getMonth() + " " + newDate.getDate());
-
-                // Value
-                values.push(element.value);
-            });
-
-            setState({
-                labels: labels,
-                datasets: [
-                    {
-                        label: "Temperatuur",
-                        backgroundColor: "rgb(1, 1, 132)",
-                        borderColor: "rgb(255, 99, 132)",
-                        data: [100, 10, 5, 25, 23, 50, 55],
-                    },
-                ],
-            });
-        }
-        );
-    console.log(data);
+    );
 }
 
-export default Dashboard
+export default Dashboard;
