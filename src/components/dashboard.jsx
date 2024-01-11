3bd710cbdfd0808e2ad82875223178d011aed216
Thijs <thijs@teerhuis.eu>
2024-01-11 23:26:56 +0100

    WP - showsmore dynamic data in the front-end - Got rid of public data object and replaced it with state

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -3,37 +3,23 @@ import Chart from 'chart.js/auto'; // https://stackoverflow.com/questions/677276
 import { Line } from "react-chartjs-2";
 import React, { useState, useEffect } from "react";
 
-let data = {
-    airPressure: [],
-    CO2: [],
-    weight: [],
-    humidity: [],
-    presences: [],
-    temperature: [],
-};
-
 function Dashboard() {
+
+    const initialDatasetState = {
+        labels: ["Loading..."],
+        datasets: [
+            {
+                label: "Loading data",
+                borderColor: "black",
+                data: [],
+            },
+        ],
+    };
+
     let [state, setState] = useState({
-        temperature: {
-            labels: ["Loading..."],
-            datasets: [
-                {
-                    label: "Loading data",
-                    borderColor: "black",
-                    data: [0, 10, 5, 2, 20, 30, 45],
-                },
-            ],
-        },
-        CO2: {
-            labels: ["Loading..."],
-            datasets: [
-                {
-                    label: "Loading data",
-                    borderColor: "black",
-                    data: [0, 10, 5, 2, 20, 30, 45],
-                },
-            ],
-        },
+        temperature: { ...initialDatasetState },
+        CO2: { ...initialDatasetState },
+        pressure: { ...initialDatasetState },
     });
 
     useEffect(() => {
@@ -42,138 +28,139 @@ function Dashboard() {
 
     // Fetch data from the API
     function getData() {
-        // Empty the arrays
-        data.airPressure = [];
-        data.CO2 = [];
-        data.weight = [];
-        data.humidity = [];
-        data.presences = [];
-        data.temperature = [];
-
-        // Get the data from the API
         fetch("http://avans.duckdns.org:1880/sensor?limit=10")
+            // Change response to JSON
             .then((response) => response.json())
+
             .then((result) => {
-                // Set the new data in the arrays
-                result.forEach((r) => {
-                    data.airPressure.push(r.decoded_payload.luminosity_3 / 100);
-                    data.CO2.push(r.decoded_payload.luminosity_5 * 10);
-                    data.weight.push(r.decoded_payload.luminosity_6 / 10);
-                    data.humidity.push(r.decoded_payload.relative_humidity_1);
-                    data.presences.push(r.decoded_payload.relative_humidity_4);
-                    data.temperature.push({
-                        key: r.time,
-                        value: r.decoded_payload.temperature_2 / 10,
-                    });
+                let time = [];
+                let temperatures = [];
+                let pressure = [];
+                let CO2 = [];
+
+                result.forEach((currentItem) => {
+                    // Change time format
+                    const timeString = changeDateFormat(currentItem.time);
+                    time.push(timeString);
+    
+                    temperatures.push(currentItem.decoded_payload.temperature_2 / 10);
+                    pressure.push(currentItem.decoded_payload.luminosity_3 / 100);
+                    CO2.push(currentItem.decoded_payload.luminosity_5 * 10);
                 });
 
-                // Update the temperature diagram
-                temperatureDiagramUpdate();
+
+                newUpdateDataset("temperature", "temperatuur", time, temperatures);
+                newUpdateDataset("pressure", "Druk", time, pressure);
+                newUpdateDataset("CO2", "CO2", time, CO2);
             });
     }
 
-    // Update the temperature diagram
-    function temperatureDiagramUpdate() {
-        // Transform the data
-        let labels = [];
-        let values = [];
+    // Changes date format
+    function changeDateFormat(date) {
+        let dateObject = new Date(date);
+        let newDate = dateObject.getDate() + "/" + (dateObject.getMonth() + 1) + " " + dateObject.getHours() + ":" + dateObject.getMinutes()
+        return newDate;
+    }
 
-        data.temperature.forEach((element) => {
-            // Key (time)
-            let newDate = new Date(element.key);
-            labels.push(
-                newDate.getDate() +
-                "/" +
-                newDate.getMonth() +
-                1 +
-                " " +
-                newDate.getHours() +
-                ":" +
-                newDate.getMinutes()
-            );
-
-            // Value
-            values.push(element.value);
-        });
-
-        // Update the state
+    // Update a object in the state
+    function newUpdateDataset(property, label, time, value) {
         setState((prevState) => ({
             ...prevState,
-            temperature: {
-                ...prevState.temperature,
-                labels: labels,
+            [property]: {
+                ...prevState[property],
                 datasets: [
                     {
-                        label: "Temperatuur",
-                        data: values,
+                        label: label,
+                        data: value,
                     },
                 ],
+                labels: time,
             },
         }));
     }
 
     return (
-        <div className="container">
-            <div className="row">
-                <div className="col-12">
-                    <h1>Dashboard</h1>
-                    {/* Select torens */}
-                    <select name="Torens" id="torens" className="select">
-                        <option value="toren1">toren</option>
-                        <option value="toren2">toren2</option>
-                        <option value="toren3">toren3</option>
-                        <option value="toren4">toren4</option>
-                    </select>
+        <>
+            <div className="container">
+                <div className="row">
+                    <div className="col-12">
+                        <h1>Dashboard</h1>
+                        {/* Select torens */}
+                        <select name="Torens" id="torens" className="select">
+                            <option value="toren1">toren</option>
+                            <option value="toren2">toren2</option>
+                            <option value="toren3">toren3</option>
+                            <option value="toren4">toren4</option>
+                        </select>
 
-                    {/* Select huisjes */}
-                    <select name="Huisjes" id="huisjes" className="select">
-                        <option value="Huisje1">Huisje1</option>
-                        <option value="Huisje2">Huisje2</option>
-                        <option value="Huisje3">Huisje3</option>
-                        <option value="Huisje4">Huisje4</option>
-                    </select>
+                        {/* Select huisjes */}
+                        <select name="Huisjes" id="huisjes" className="select">
+                            <option value="Huisje1">Huisje1</option>
+                            <option value="Huisje2">Huisje2</option>
+                            <option value="Huisje3">Huisje3</option>
+                            <option value="Huisje4">Huisje4</option>
+                        </select>
+                    </div>
+                </div>
+
+                <div className="row">
+                    {/* temp */}
+                    <div className="col-md-4">
+                        <div className="wr-diagram diagram-temperature">
+                            <h2>Temperatuur</h2>
+                            <span className="wr-temperature"><span id="temperature">{state.temperature.datasets[0].data[0] || 0}</span><sup>C</sup></span>
+                            <div className="wr-slider">
+                                <div className="scale">
+                                    <span>-10</span><span>40</span>
+                                </div>
+                                <input readOnly type="range" min="0" max="40" step="0.1" value={state.temperature.datasets[0].data[0] || 0} className="slider" id="myRange" />
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* Pressure */}
+                    <div className="col-md-4">
+                        <div className="wr-diagram">
+                            <h2>Druk</h2>
+                            <span className="wr-pressure">{state.pressure.datasets[0].data[0]}<sup>C</sup></span>
+                            <div className="wr-slider">
+                                <div className="scale">
+                                    <span>0.5</span><span>1.5</span>
+                                </div>
+                                <input readOnly type="range" min="0.5" max="1.5" step="0.1" value={state.pressure.datasets[0].data[0] || 0} className="slider" id="myRange" />
+                            </div>
+                        </div>
+                    </div>
+
+                    {/* CO2 */}
+                    <div className="col-md-4">
+                        <div className="wr-diagram">
+                            <h2>Co2</h2>
+                            <span className="wr-CO2">{state.temperature.datasets[0].data[0]}<sup>C</sup></span>
+                            <div className="wr-slider">
+                                <div className="scale">
+                                    <span>0</span><span>1</span>
+                                </div>
+                                <input readOnly type="range" min="0" max="2000" step="0.1" value={500} className="slider" id="myRange" />
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>
 
-            <div className="row">
-                {/* temp */}
-                <div className="col-md-4">
-                    <div className="wr-diagram">
-                        <h3>Temperatuur</h3>
-                        <div class="slidecontainer">
-                            <input type="range" min="1" max="100" value="80" class="slider" id="myRange"/>
+            <div className="container">
+                <div className="row">
+                    {/* Temp line chart */}
+                    <div className="col">
+                        <div className="wr-diagram">
+                            <h3>Temperatuur</h3>
+                            <Line className="temperature" data={state.temperature} />
                         </div>
                     </div>
                 </div>
-
-                {/* Pressure */}
-                <div className="col-md-4">
-                    <div className="wr-diagram">
-                        <h3>Druk</h3>
-                    </div>
-                </div>
-
-                {/* CO2 */}
-                <div className="col-md-4">
-                    <div className="wr-diagram">
-                        <h3>Co2</h3>
-                    </div>
-                </div>
-
             </div>
-
-            <div className="row">
-                {/* Temp line chart */}
-                <div className="col-xl-6 col-sm-12">
-                    <div className="wr-diagram">
-                        <h3>Temperatuur</h3>
-                        <Line className="temperature" data={state.temperature} />
-                    </div>
-                </div>
-            </div>
-
-
-        </div>
+        </>
     );
 }
 
