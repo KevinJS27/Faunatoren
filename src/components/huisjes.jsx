3732e3b3dff53002c22a5b703d413978a7f668c2
19isabella <ijj.dejong@student.avans.nl>
2024-01-18 18:16:21 +0100

    added dialog window + huisjes naam

--- src/components/huisjes.jsx
+++ src/components/huisjes.jsx
@@ -4,19 +4,19 @@ import './../style/components/huisjes.scss';
 
 const Huisjes = () => {
   const [huisjes, setHuisjes] = useState([
-    { id: 1, toren: 'Toren 1', locatie: 'Amsterdam', aantal: 3 },
-    { id: 2, toren: 'Toren 2', locatie: 'Utrecht', aantal: 5 },
+    { id: 1, toren: 'Toren 1',naam: 'Huisje 1' },
+    { id: 2, toren: 'Toren 2', naam: 'Huisje 2' },
   ]);
 
   const [editHuisje, setEditHuisje] = useState(null);
-  const [nieuwHuisje, setNieuwHuisje] = useState({ toren: '', locatie: '', aantal: 0 });
+  const [nieuwHuisje, setNieuwHuisje] = useState({ toren: '', naam: ''});
 
   const [showDeleteDialog, setShowDeleteDialog] = useState(false);
   const [deleteHuisjeId, setDeleteHuisjeId] = useState(null);
 
   const handleToevoegen = () => {
     setHuisjes([...huisjes, { id: Date.now(), ...nieuwHuisje }]);
-    setNieuwHuisje({ toren: '', locatie: '', aantal: 0 });
+    setNieuwHuisje({ toren: '', naam: '' });
   };
 
   const handleBijwerken = () => {
@@ -45,53 +45,65 @@ const Huisjes = () => {
   };
 
   return (
-    <div className="huisjes-container">
-      <h1>Huisjes</h1>
-      <div className="huisjes-list">
-        {huisjes.map(huis => (
-          <div key={huis.id} className="huisje-item">
-            <span>{huis.toren}</span>
-            <span>{huis.locatie}</span>
-            <span>{huis.aantal} huisjes</span>
-            <button onClick={() => setEditHuisje(huis)}>Bewerken</button>
-            <button onClick={() => handleVerwijderen(huis.id)}>Verwijderen</button>
+    <>
+      <div className="container huisjes-container">
+        <div className="row">
+
+          {/* View */}
+          <div className="col-12">
+            <h1>Huisjes</h1>
+            <div className="huisjes-list">
+              {huisjes.map(huis => (
+                <div key={huis.id} className="huisje-item">
+                  <span>{huis.toren}</span>
+                  <span>{huis.naam}</span>
+                  <button onClick={() => setEditHuisje(huis)}>Bewerken</button>
+                  <button onClick={() => handleVerwijderen(huis.id)}>Verwijderen</button>
+                </div>
+              ))}
+            </div>
           </div>
-        ))}
-      </div>
-      <div className="huisje-form">
-        <h2>{editHuisje ? 'Bewerk Huisje' : 'Voeg Huisje Toe'}</h2>
-        <label>Toren:</label>
-        <input
-          type="text"
-          value={editHuisje ? editHuisje.toren : nieuwHuisje.toren}
-          onChange={e => (editHuisje ? setEditHuisje({ ...editHuisje, toren: e.target.value }) : setNieuwHuisje({ ...nieuwHuisje, toren: e.target.value }))}
-        />
-        <label>Locatie:</label>
-        <input
-          type="text"
-          value={editHuisje ? editHuisje.locatie : nieuwHuisje.locatie}
-          onChange={e => (editHuisje ? setEditHuisje({ ...editHuisje, locatie: e.target.value }) : setNieuwHuisje({ ...nieuwHuisje, locatie: e.target.value }))}
-        />
-        <label>Aantal Huisjes:</label>
-        <input
-          type="number"
-          value={editHuisje ? editHuisje.aantal : nieuwHuisje.aantal}
-          onChange={e => (editHuisje ? setEditHuisje({ ...editHuisje, aantal: parseInt(e.target.value) }) : setNieuwHuisje({ ...nieuwHuisje, aantal: parseInt(e.target.value) }))}
-        />
-        {editHuisje ? (
-          <button onClick={handleBijwerken}>Bijwerken</button>
-        ) : (
-          <button onClick={handleToevoegen}>Toevoegen</button>
-        )}
+        </div>
+        <hr />
+        <div className="row">
+
+          {/* View */}
+          <div className="col-12">
+            <div className="huisje-form">
+              <h2>{editHuisje ? 'Bewerk Huisje' : 'Voeg Huisje Toe'}</h2>
+              <label>Toren:</label>
+              <input
+                type="text"
+                value={editHuisje ? editHuisje.toren : nieuwHuisje.toren}
+                onChange={e => (editHuisje ? setEditHuisje({ ...editHuisje, toren: e.target.value }) : setNieuwHuisje({ ...nieuwHuisje, toren: e.target.value }))}
+              />
+              <label>Naam:</label>
+              <input
+                type="text"
+                value={editHuisje ? editHuisje.naam : nieuwHuisje.naam}
+                onChange={e => (editHuisje ? setEditHuisje({ ...editHuisje, naam: e.target.value }) : setNieuwHuisje({ ...nieuwHuisje, naam: e.target.value }))}
+              />
+              {editHuisje ? (
+                <button onClick={handleBijwerken}>Bijwerken</button>
+              ) : (
+                <button onClick={handleToevoegen}>Toevoegen</button>
+              )}
+            </div>
+          </div>
+        </div>
       </div>
       {showDeleteDialog && (
-        <dialog open>
+        <>
+        <div className="dialog-backdrop" />
+        <dialog open={showDeleteDialog}>
           <p>Weet je zeker dat je dit huisje wilt verwijderen?</p>
           <button onClick={handleConfirmVerwijderen}>Ja</button>
           <button onClick={handleCancelVerwijderen}>Nee</button>
         </dialog>
-      )}
-    </div>
+        </>
+      )};
+
+    </>
   );
 };
 
