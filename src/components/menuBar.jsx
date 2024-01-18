bfb8c9673ea0bba399faceb05f36465a61b60502
19isabella <ijj.dejong@student.avans.nl>
2024-01-18 14:33:08 +0100

    x

--- src/components/menuBar.jsx
+++ src/components/menuBar.jsx
@@ -1,13 +1,12 @@
-import './../style/grid/_index.scss';
-import './../style/components/menuBar.scss';
-import faunaTorenLogo from './../assets/faunatoren_logo.png';
-import userIcon from './../assets/userIcon.svg';
+import './../style/grid/_index.scss'
+import './../style/components/menuBar.scss'
+import faunaTorenLogo from './../assets/faunatoren_logo.png'
+import userIcon from './../assets/userIcon.svg'
 
-function MenuBar({onMenuItemClick}) {
 
-    const handleMenuItemClick = (menuItem) => {
-        onMenuItemClick(menuItem);
-    };
+function MenuBar() {
+
+    
 
     return (
         <div className="container-fluid menubar">
@@ -16,23 +15,23 @@ function MenuBar({onMenuItemClick}) {
                     <div className="row">
                         <menu className="col-sm menu">
                             <img src={faunaTorenLogo} className="logo" alt="Faunatoren logo" />
-                            <li onClick={() => handleMenuItemClick('dashboard')}>Dashboard</li>
-                            <li onClick={() => handleMenuItemClick('torens')}>Torens</li>
-                            <li onClick={() => handleMenuItemClick('huisjes')}>Huisjes</li>
-                            <li>
-                                <img
-                                    onClick={() => handleMenuItemClick('user')}
-                                    src={userIcon}
-                                    className="User"
-                                    alt="Gebruiker informatie"
-                                />
-                            </li>
+                            <li>Dashboard</li>
+                            <li>Torens</li>
+                            <li>Huisjes</li>
+                            <li>icon</li>
+                            <li><img onClick={userProfile()} src={userIcon} className="User" alt="Gebruiker informatie" /></li>
                         </menu>
                     </div>
                 </div>
             </div>
         </div>
-    );
+    )
+    }
+
+function userProfile(){
+    console.log("few");
 }
 
-export default MenuBar;
+
+
+export default MenuBar
