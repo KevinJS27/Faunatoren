8e7fd0179d9227c194a908d39e7be8f6b7e5c9fe
KevinJS27 <kj.schollen@student.avans.nl>
2024-01-20 22:04:56 +0100

    geen rare redirect

--- src/components/logs.jsx
+++ src/components/logs.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Auth0Client } from '@auth0/auth0-spa-js';
+import "./../style/components/logs.scss";
 
 const LogsComponent = () => {
     const [mbdLogs, setMBDLogs] = useState([]);
@@ -24,56 +25,60 @@ const LogsComponent = () => {
 
 
     return (
-        <div>
-            <h2>MongoDB Logs</h2>
-            <table>
-                {mbdLogs.map((log, index) => (
-                    <tr>
-                        <td>
-                            <p key={index}>{log._id}</p>
-                        </td>
-                        <td>
-                            <p key={index}>{log.ip}</p>
-                        </td>
-                        <td>
-                            <p key={index}>{log.username}</p>
-                        </td>
-                        <td>
-                            <p key={index}>{log.action}</p>
-                        </td>
-                    </tr>
-                ))}
-            </table>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h2>MongoDB Logs</h2>
+                    <table className='logsTable MBD'>
+                        {mbdLogs.map((log, index) => (
+                            <tr>
+                                <td>
+                                    <p key={index}>{log._id}</p>
+                                </td>
+                                <td>
+                                    <p key={index}>{log.ip}</p>
+                                </td>
+                                <td>
+                                    <p key={index}>{log.username}</p>
+                                </td>
+                                <td>
+                                    <p key={index}>{log.action}</p>
+                                </td>
+                            </tr>
+                        ))}
+                    </table>
 
-            <h2>Auth0 Logs</h2>
-            <table>
-                {logs.map((log, index) => (
-                    <tr>
-                        <td>
-                            <p key={index}>{log._id}</p>
-                        </td>
-                        <td>
-                            <p key={index}>{log.type}</p>
-                        </td>
-                        <td>
-                            <p key={index}>{log.date}</p>
-                        </td>
-                        <td>
-                            <p key={index}>{log.description}</p>
-                        </td>
-                        <td>
-                            <p key={index}>{log.ip}</p>
-                        </td>
-                        <td>
-                            <p key={index}>{log.user_id}</p>
-                        </td>
-                        <td>
-                            <p key={index}>{log.user_name}</p>
-                        </td>
-                    </tr>
-                ))}
-            </table>
-            <pre>{JSON.stringify(logs, null, 2)}</pre>
+                    <h2>Auth0 Logs</h2>
+                    <table className='logsTable AUTH0'>
+                        {logs.map((log, index) => (
+                            <tr>
+                                <td>
+                                    <p key={index}>{log._id}</p>
+                                </td>
+                                <td>
+                                    <p key={index}>{log.type}</p>
+                                </td>
+                                <td>
+                                    <p key={index}>{log.date}</p>
+                                </td>
+                                <td>
+                                    <p key={index}>{log.description}</p>
+                                </td>
+                                <td>
+                                    <p key={index}>{log.ip}</p>
+                                </td>
+                                <td>
+                                    <p key={index}>{log.user_id}</p>
+                                </td>
+                                <td>
+                                    <p key={index}>{log.user_name}</p>
+                                </td>
+                            </tr>
+                        ))}
+                    </table>
+                    <pre>{JSON.stringify(logs, null, 2)}</pre>
+                </div>
+            </div>
         </div>
     );
 };
